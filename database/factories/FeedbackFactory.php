52fdd5bb279c86e0adbf0c13ae64d93ea120d9d2
binhnguyen <nguyenvanbinh3479@gmail.com>
2018-11-03 15:49:40 +0700

    modify factories

--- database/factories/FeedbackFactory.php
+++ database/factories/FeedbackFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Feedback::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'content' => $faker->content,
+        'user_id' => $faker->user_id
     ];
 });
--- database/factories/ThemeFactory.php
+++ database/factories/ThemeFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Theme::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'description' => $faker->paragraph,
+        'content' => $faker->content,
+        'thumbnail'=> $faker->thumnail,
     ];
 });
